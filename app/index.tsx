18da182044c4fee4abbb61390172e6fda5677bc6
Vinicius Oliveira <fukhaos@gmail.com>
2024-12-14 13:47:43 -0300

    changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,17 @@
 import styles from "@/styles";
+import { useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const [desculpa, setDesculpa] = useState("")
+
+  const gerarDesculpa = () => {
+    if (desculpa.length < 5) {
+      alert("Desculpe, o evento precisa ter mais de 5 caracteres")
+      return;
+    }
+  }
+
   return (
     <View
       style={styles.container}
@@ -9,10 +19,12 @@ export default function Index() {
       <Text style={styles.titulo}>Desculpator 300</Text>
       <Text style={styles.subtitulo}>Sua maquina de desculpas profissional</Text>
       <TextInput
+        onChangeText={setDesculpa}
+        value={desculpa}
         style={styles.input}
         placeholder="Digite o evento que você quer evitar ..."></TextInput>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={gerarDesculpa}>
         <Text style={styles.buttonText}>Gerar desculpas infalível!</Text>
       </TouchableOpacity>
 
